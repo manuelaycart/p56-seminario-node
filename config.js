9aebd418eaec7781881da578a5e9618ac8054062
Manuel Aycart Armijos <manuel.aycart.ec@gmail.com>
2020-10-02 00:05:51 -0500

    proyecto Seminario

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.rxzjb.gcp.mongodb.net/utups?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://utups:ups2020@cluster0.lggop.gcp.mongodb.net/utups?retryWrites=true&w=majority',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
